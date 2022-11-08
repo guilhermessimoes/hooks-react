e0ee9dee6428d384d8e1040fe8f483a0b1eef5ab
Guilherme Simões <guilhermejunios@gmail.com>
2022-11-08 10:04:53 -0300

    UseState exercicio

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -4,6 +4,7 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('Inicial...')
 
     return (
         <div className="UseState">
@@ -23,7 +24,10 @@ const UseState = (props) => {
                     </div>
                 </div>
             </div>
-            <SectionTitle title="Exercício #01"/>
+            <SectionTitle title="Exercício #02"/>
+            <input type="text" className="input" value={name} onChange={e => setName(e.target.value)}/>
+            <span className="text">{name}</span>
+
         </div>
     )
 }
